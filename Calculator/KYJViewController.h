c9344bd7c48426ff55f138e398751f3de2499629
Kevin Jiang <kevinyjiang1@gmail.com>
2013-01-16 22:49:46 -0500

    Initial Commit

---
+++ Calculator/KYJViewController.h
@@ -0,0 +1,13 @@
+//
+//  KYJViewController.h
+//  Calculator
+//
+//  Created by Kevin Jiang on 1/16/13.
+//  Copyright (c) 2013 Kevin Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KYJViewController : UIViewController
+
+@end
---
+++ CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by Kevin Jiang on 1/16/13.
+//  Copyright (c) 2013 Kevin Jiang. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
